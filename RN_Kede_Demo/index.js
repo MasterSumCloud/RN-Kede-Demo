1ab7bc4af87deda79c9c9222075b35988bb67c56
Mac_py <summer.p.y@foxmail.com>
2018-09-11 16:39:39 +0800

    Nv 设置

--- RN_Kede_Demo/index.js
+++ RN_Kede_Demo/index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import RootScene from './src/RootScene';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => RootScene);
---
+++ RN_Kede_Demo/src/cart/CartScreen.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+
+import {
+	View,
+	Text,
+	StyleSheet
+} from 'react-native';
+
+class CartScreen extends Component {
+	render(){
+		return(
+			<View style={styles.container}>
+				<Text style={styles.text}>Cart Screen</Text>
+			</View>
+		)
+	}
+}
+
+export default CartScreen;
+
+
+const styles = StyleSheet.create({
+	container:{
+		flex:1,
+		justifyContent:'center',
+		alignItems:'center',
+	},
+	text:{
+		color:'#35caca',
+		fontSize:30
+	}
+});
